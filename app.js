d9c0cbd2001ac347ca72011c962c14f5ab534df1
FLaXCF <kakakcantqq@gmail.com>
2023-11-18 12:59:34 +0700

    maybe

--- app.js
+++ app.js
@@ -16,22 +16,17 @@ app.listen(port, () => {
 
 // Fungsi untuk menghasilkan tabel kebenaran
 function generateTruthTable() {
-  const truthTable = [];
-
-  // Loop untuk semua kemungkinan kombinasi nilai A, B, dan C (0 atau 1)
-  for (let a = 0; a <= 1; a++) {
-    for (let b = 0; b <= 1; b++) {
-      for (let c = 0; c <= 1; c++) {
-        const bPrime = !b; // B'
-        const cPrime = !c; // C'
-        const bCPrime = bPrime && cPrime; // B'C'
-        const bC = b && c; // BC
-        const result = a && (bCPrime || bC); // Z = A(B'C' + BC)
-        
-        truthTable.push({ a, b, c, bPrime, cPrime, bCPrime, bC, result });
+    const truthTable = [];
+  
+    // Loop untuk semua kemungkinan kombinasi nilai A, B, dan C (0 atau 1)
+    for (let a = 0; a <= 1; a++) {
+      for (let b = 0; b <= 1; b++) {
+        for (let c = 0; c <= 1; c++) {
+          const result = a ^ b; // Z = A XOR B
+          truthTable.push({ a, b, c, result });
+        }
       }
     }
-  }
-
-  return truthTable;
+  
+    return truthTable;
 }
