dbb58f2d9ff3bb42222062c95867215743fce87e
FLaXCF <kakakcantqq@gmail.com>
2023-11-18 13:09:54 +0700

    uh

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  // Render HTML page with truth table data
   res.render('index', { truthTable: generateTruthTable() });
 });
 
@@ -14,22 +13,21 @@ app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-// Function to generate truth table
 function generateTruthTable() {
-    const truthTable = [];
-  
-    // Loop through all possible combinations of A, B, and C (0 or 1)
-    for (let a = 0; a <= 1; a++) {
-      for (let b = 0; b <= 1; b++) {
-        for (let c = 0; c <= 1; c++) {
-          const bPrime = !b; // B'
-          const cPrime = !c; // C'
-          const result = a && (bPrime && cPrime || (b && c)); // Z = A(B'C' + BC)
-  
-          truthTable.push({ a, b, c, bPrime, cPrime, result });
-        }
+  const truthTable = [];
+  for (let a = 0; a <= 1; a++) {
+    for (let b = 0; b <= 1; b++) {
+      for (let c = 0; c <= 1; c++) {
+        const bPrime = !b; 
+        const cPrime = !c; 
+        const bCPrime = bPrime && cPrime; 
+        const bC = b && c; 
+        const result = a && (bCPrime || bC);
+        
+        truthTable.push({ a, b, c, bPrime, cPrime, bCPrime, bC, result });
       }
     }
-  
+  }
+
   return truthTable;
 }
